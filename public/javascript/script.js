d65a41de7eb6aefbabb50fb946743003ce0d3148
Andrew Schlegel <aschlegel09@gmail.com>
2018-12-04 11:08:23 -0500

    text and animation changes

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,17 +1,25 @@
 $(document).ready(function () {
-
-    // $('#sidebarCollapse').on('click', function () {
-    //     $('#sidebar').toggleClass('active');
-    // });
-
-    // $('.carousel').carousel({
-    //     interval: 20
-    //   })
-
     // scroll fade color
     $(window).scroll(function () {
         $(".fade").css("opacity", 1 - $(window).scrollTop() / 650);
         $(".first-view").css("opacity", 1 - $(window).scrollTop() / 650);
     });
 
+    $("#optionBtn").click(function() {
+        $("#title").animate({
+            height: "220px",
+            fontSize: "80px"
+        }, 5000, function(){
+            console.log("font size changed");
+        });
+        $("#role").animate({
+            opacity: "toggle",
+            height: ["toggle", "swing"]
+        }, 5000, "linear", function() {
+            console.log("animation complete");
+        }).then
+        $("#optionBtn").animate({
+            opacity: 0
+        })
+    });
 });
